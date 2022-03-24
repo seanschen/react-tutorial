4e9db9caf8b551b97342b8af5861118b5daace95
Sean Chen <seanschen1@gmail.com>
2022-03-23 21:56:56 -0700

    Use loops instead of hardcoding to render squares in board

--- src/index.js
+++ src/index.js
@@ -23,21 +23,13 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {[...Array(3)].map((x, i) =>
+          <div className="board-row">
+            {[...Array(3)].map((x, j) =>
+              this.renderSquare(i*3+j)
+            )}
+          </div>
+        )}
       </div>
     );
   }
