8c0703206a119fb4021f110e2224656327189acc
Sean Chen <seanschen1@gmail.com>
2022-03-23 21:10:52 -0700

    Made current move bold

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ class Game extends React.Component {
         'Go to game start';
         return (
           <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            <button className={move === this.state.stepNumber ? 'button-selected' : 'button'} onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         );
     });
